da2f0526e0cdbe1c93cc8dce6f98ba4ee462bf14
Pradip Neupane <pradip.neupane@tuni.fi>
2024-10-03 16:59:13 +0300

    Read cookies

--- exercises/05_cookie/ex3/script.js
+++ exercises/05_cookie/ex3/script.js
@@ -1,11 +1,19 @@
 // TODO: Copy the setCookies function from the previous exercise
 function setCookies() {
-  
+  const text1Value = document.getElementById('text1').value;
+  const text2Value = document.getElementById('text2').value;
+  const checkboxValue = document.getElementById('checkbox').checked;
+
+  document.cookie = `text1=${text1Value}; path=/`;
+  document.cookie = `text2=${text2Value}; path=/`;
+  document.cookie = `checkbox=${checkboxValue}; path=/`;
 }
 
 // TODO: Implement the getCookie function. It should take a cookie name as an argument and return the cookie value.
 function getCookie(name) {
-
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
 
