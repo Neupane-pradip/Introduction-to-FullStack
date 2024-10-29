77a238de52bc4ec716a7cacd389e73807f69a532
Pradip Neupane <pradip.neupane@tuni.fi>
2024-10-29 17:14:43 +0200

    fix minor log function

--- exercises/08_authentication/ex5/app.js
+++ exercises/08_authentication/ex5/app.js
@@ -1,27 +1,30 @@
 'use strict';
 
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const app = express();
+(() => {
+    const express = require('express');
+    const path = require('path');
+    const indexRouter = require('./routes/index');
+    const usersRouter = require('./routes/users');
+    const app = express();
 
-// Use express's body-parser middleware
-app.use(express.urlencoded({ extended: false }));
+    // Use express's body-parser middleware
+    app.use(express.urlencoded({ extended: false }));
 
-// Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    // Routes
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter);
 
-// Start the server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+    // Start the server
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
 
-// Log function (unreachable code corrected)
-function log(error = "") {
-    console.log(`Oops! Something went wrong: ${error}`);
-    return 0;
-}
+    // Log function
+    function log(error = "") {
+        console.log(`Oops! Something went wrong: ${error}`);
+        return 0;
+    }
 
-module.exports = app;
+    // Optionally expose the log function if needed
+    module.exports = { app, log };
+})();
